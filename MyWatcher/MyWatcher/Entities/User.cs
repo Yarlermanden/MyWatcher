2aca8eb0c415c386bda68d63013ea76cb8a3fe68
Martin Meldgaard Holst <martinmeldgaardholst@Martins-MacBook-Pro.local>
2021-10-09 20:23:47 +0200

    Added initial Library project with dbcontext

---
+++ MyWatcher/MyWatcher/Entities/User.cs
@@ -0,0 +1,10 @@
+namespace MyWatcher.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ MyWatcher/MyWatcher/Services/DatabaseContext.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using MyWatcher.Entities;
+
+namespace MyWatcher.Services
+{
+    public class DatabaseContext : IDisposable
+    {
+        public DbSet<Item> Items { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<UserItem> UserItems { get; set; }
+
+        protected void OnModelCreating(ModelBuilder builder)
+        {
+
+            var items = builder.Entity<Item>().ToTable("Item");
+            var users = builder.Entity<User>().ToTable("User");
+            var services = builder.Entity<Service>().ToTable("Service");
+            var userItems = builder.Entity<UserItem>().ToTable("UserItem");
+
+            items.HasKey(item => item.Id);
+            items.Property(item => item.Id).HasColumnName("Id").ValueGeneratedOnAdd();
+
+            users.HasKey(user => user.Id);
+            users.Property(user => user.Id).HasColumnName("Id").ValueGeneratedOnAdd();
+
+            services.HasKey(service => service.Id);
+            services.Property(service => service.Id).HasColumnName("Id").ValueGeneratedOnAdd();
+            services.HasMany(s => s.Items).WithOne(i => i.Service).HasForeignKey(i => i.ServiceId);
+
+            userItems.HasKey(ui => ui.Id);
+            userItems.Property(ui => ui.Id).HasColumnName("Id").ValueGeneratedOnAdd();
+            userItems.HasOne(ui => ui.Item);
+            userItems.HasOne(ui => ui.User);
+
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
