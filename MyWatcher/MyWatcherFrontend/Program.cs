84aa06e3ae35c368a59cbc12d5e7890dd73c30c2
Martin Meldgaard Holst <martinmeldgaardholst@Martins-MacBook-Pro.local>
2021-10-12 00:37:27 +0200

    Clean up and added some initial html and setup of pages

--- MyWatcher/MyWatcherFrontend/Program.cs
+++ MyWatcher/MyWatcherFrontend/Program.cs
@@ -20,19 +20,8 @@ namespace MyWatcherFrontend
             builder.RootComponents.Add<HeadOutlet>("head::after");
             
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000") });
             builder.Services.AddSingleton<ICommunicationService, CommunicationService>();
             builder.Services.AddSingleton<IApiService, ApiService>();
-            //builder.Services.AddApiAuthorization("webApi", client => client.BaseAddress = new Uri("localhost:5000"));
-
-            /*
-            builder.Services.AddHttpClient("WebAPI", 
-                    client => client.BaseAddress = new Uri("https://www.example.com/base"))
-                .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
-
-            builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
-                .CreateClient("WebAPI"));
-            */
 
             await builder.Build().RunAsync();
         }
