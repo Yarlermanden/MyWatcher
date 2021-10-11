b4065ff997bdd1e55df936b75b691d9eeabb250b
Martin Holst <mmho@itu.dk>
2021-10-12 00:37:27 +0200

    Clean up and added some initial html and setup of pages

--- MyWatcher/MyWatcherFrontend/Services/CommunicationService.cs
+++ MyWatcher/MyWatcherFrontend/Services/CommunicationService.cs
@@ -7,24 +7,19 @@ namespace MyWatcherFrontend.Services
 {
     public interface ICommunicationService
     {
-        //public Task<IRestResponse> SendGetRequest(string endpoint, object item);
         public Task<HttpResponseMessage> SendGetRequest(string endpoint, object item);
         public Task PostItemRequest(string endpoint, object item);
     }
     
     public class CommunicationService : ICommunicationService
     {
-        //private readonly RestClient _client;
         private readonly HttpClient _client;
 
         public CommunicationService()
         {
-            //_client = new RestClient("http://localhost:5000");
-            //_client = new HttpClient("http://localhost:5000");
             _client = new HttpClient();
         }
         
-        //public async Task<IRestResponse> SendRequestToApi(string endpoint, object item, Method requestType)
         public async Task<HttpResponseMessage> SendRequestToApi(string endpoint, object item, Method requestType)
         {
             var request = new RestRequest("http://localhost:5000" + endpoint, requestType)
@@ -32,20 +27,17 @@ namespace MyWatcherFrontend.Services
                 RequestFormat = DataFormat.Json
             };
             request.AddJsonBody(item);
-            //var response = await _client.ExecuteAsync(request);
 
             switch (requestType)
             {
                 case Method.GET:
                     return await _client.GetAsync("https://localhost:5001" + endpoint);
                 case Method.POST:
-                    //return await _client.PostAsync()
                 default:
                     return null;
             }
         }
 
-        //public async Task<IRestResponse> SendGetRequest(string endpoint, object item)
         public async Task<HttpResponseMessage> SendGetRequest(string endpoint, object item)
         {
             var response = await SendRequestToApi(endpoint, item, Method.GET);
