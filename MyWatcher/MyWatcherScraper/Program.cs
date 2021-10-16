74daa3b81f3a365bf4274d8a45ba52010db92082
Martin Holst <mmho@itu.dk>
2021-10-16 20:25:28 +0200

    Added MyWatcherScraper. Only initial setup of application

---
+++ MyWatcher/MyWatcherScraper/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+
+namespace MyWatcherScraper
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "";
+            var isDevelopment = environment == Environments.Development;
+            if (isDevelopment)
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
+                    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+                    .Enrich.FromLogContext().WriteTo.Console()
+                    //.WriteTo.File("log.txt", rollingInterval: RollingInterval.Day, restrictedToMinimumLevel: LogEventLevel.Information)
+                    .CreateLogger();
+            }
+            else
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Error)
+                    .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Error)
+                    .Enrich.FromLogContext().CreateLogger();
+            }
+
+            try
+            {
+                Log.Information("Starting up");
+                host.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+            return 0;
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args).UseSerilog()
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    var environment = hostingContext.HostingEnvironment.EnvironmentName;
+                    config.SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+                    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{environment}.json", optional: false, reloadOnChange: true)
+                        .AddEnvironmentVariables();
+                })
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        }
+    }
+}
