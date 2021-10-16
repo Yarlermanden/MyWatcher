fda0cdff6e46e89e7635a8a7d74b30114f257a2d
Martin Holst <mmho@itu.dk>
2021-10-17 01:10:31 +0200

    Changed pricing parsing from using '.' to ','

--- MyWatcher/MyWatcherScraper/Services/ExtractService.cs
+++ MyWatcher/MyWatcherScraper/Services/ExtractService.cs
@@ -11,25 +11,38 @@ namespace MyWatcherScraper.Services
     
     public class ExtractService : IExtractService
     {
-        private Regex _pricerunnerPriceRegex;
+        private Regex _pricerunnerPriceRangeRegex;
+        private Regex _pricerunnerPriceSingleRegex;
         
         public ExtractService()
         {
-            _pricerunnerPriceRegex = new Regex(@"Sammenlign priser fra (\d+\.?\d*)( (.{1-5})? til)?");
+            _pricerunnerPriceRangeRegex = new Regex(@"Sammenlign priser fra (\d+(\.?\d*)*)( (.{1-5})? til)?");
+            _pricerunnerPriceSingleRegex = new Regex(@"Pris (\d+(\.?\d*)*) ?");
         }
 
         public async Task<double> ExtractPriceFromPricerunner(HtmlAgilityPack.HtmlDocument html)
         {
-            var match = _pricerunnerPriceRegex.Match(html.DocumentNode.InnerText);
+            var match = _pricerunnerPriceRangeRegex.Match(html.DocumentNode.InnerText);
             if (match.Success)
             {
                 Console.WriteLine(match.Groups[1]);
-                var success = double.TryParse(match.Groups[1].Value, out var price);
+                var success = double.TryParse(match.Groups[1].Value.Replace(".", ","), out var price);
                 if (success) return price;
                 else Console.WriteLine("Failed parsing price to double");
                 //Console.WriteLine(match.Groups[2]);
             }
-            else Console.WriteLine("No match");
+            else
+            {
+                match = _pricerunnerPriceSingleRegex.Match(html.DocumentNode.InnerText);
+                if (match.Success)
+                {
+                    Console.WriteLine(match.Groups[1]);
+                    var success = double.TryParse(match.Groups[1].Value.Replace(".", ","), out var price);
+                    if (success) return price;
+                    else Console.WriteLine("Failed parsing price to double");
+                }
+                Console.WriteLine("No match");
+            }
             return -1;
         }
     }
